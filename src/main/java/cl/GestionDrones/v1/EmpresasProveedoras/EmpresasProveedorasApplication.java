dd357ae205458a331f699aec95ebed4a23adb41f
Lilyennd <victoriariffo21@gmail.com>
2026-05-22 11:43:11 -0400

    inicio

---
+++ src/main/java/cl/GestionDrones/v1/EmpresasProveedoras/EmpresasProveedorasApplication.java
@@ -0,0 +1,13 @@
+package cl.GestionDrones.v1.EmpresasProveedoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresasProveedorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresasProveedorasApplication.class, args);
+	}
+
+}
